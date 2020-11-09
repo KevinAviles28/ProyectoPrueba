b0601e08fb838800bd5c2f1d69baf745fa3d56e8
KevinAviles28 <bejarano.a.kevin@gmail.com>
2020-11-08 22:31:35 -0300

    hice una calculadora

---
+++ otraCalculadora.js
@@ -0,0 +1,15 @@
+let calculadora={
+    sumar: function(a,b){
+        return a+b;
+    },
+    restar: function(a,b){
+        return a-b;
+    },
+    multiplicar: function(a,b){
+        return a*b;
+    },
+    dividir: function(a,b){
+        return a/b;
+    }
+}
+console.log(calculadora.multiplicar(4,2));
